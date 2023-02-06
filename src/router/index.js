df38d359de8eeed6aa88580ae0df42a8c9c2a6ec
DevHumbleChris <dev.humblechris254@gmail.com>
2023-02-06 11:53:36 +0300

    updated the routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,57 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import { auth } from '@/firebaseConfig'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import { auth } from "@/firebaseConfig";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
       meta: {
-        isUnderConstruction: true
-      }
+        isUnderConstruction: true,
+      },
     },
     {
-      path: '/under-construction',
-      name: 'under-construction',
-      component: () => import('../views/UnderConstruction.vue')
+      path: "/under-construction",
+      name: "under-construction",
+      component: () => import("../views/UnderConstruction.vue"),
     },
     {
-      path: '/auth/login',
-      name: 'login',
-      component: () => import('../views/auth/LoginView.vue')
+      path: "/auth/login",
+      name: "login",
+      component: () => import("../views/auth/LoginView.vue"),
     },
     {
-      path: '/auth/signup',
-      name: 'signup',
-      component: () => import('../views/auth/SignupView.vue')
+      path: "/auth/signup",
+      name: "signup",
+      component: () => import("../views/auth/SignupView.vue"),
     },
     {
-      path:'/account/admin',
-      name: 'admin',
+      path: "/account/admin",
+      name: "admin",
       component: () => import("../views/admin/AdminView.vue"),
-      meta: { requiresAuth: true }
+      meta: { requiresAuth: true, isUnderConstruction: true },
     },
     {
-      path: '/account/member',
-      name: 'member',
+      path: "/account/member",
+      name: "member",
       component: () => import("../views/member/MemberView.vue"),
-      meta: { requiresAuth: true }
-    }
-  ]
-})
+      meta: { requiresAuth: true, isUnderConstruction: true },
+    },
+  ],
+});
 
 router.beforeEach((to, from, next) => {
-  const isUnderConstruction = to.matched.some(record => record.meta.isUnderConstruction)
-  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-  const isAuthenticated = auth.currentUser
+  const isUnderConstruction = to.matched.some(
+    (record) => record.meta.isUnderConstruction
+  );
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const isAuthenticated = auth.currentUser;
 
-  if (isUnderConstruction) next({ name: 'under-construction' })
+  if (requiresAuth && isUnderConstruction && !isAuthenticated) next({ name: "under-construction" })
   else next()
-})
+});
 
-export default router
+export default router;
